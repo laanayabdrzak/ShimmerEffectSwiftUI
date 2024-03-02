93c11c4fdfecac06ee5162ca3d3fd586a7d1ad59
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-02 21:34:54 +0100

    Initial Commit

---
+++ ShimmerEffectSwiftUI/ShimmerEffectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectSwiftUIApp.swift
+//  ShimmerEffectSwiftUI
+//
+//  Created by LAANAYA Abderrazak on 2/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
